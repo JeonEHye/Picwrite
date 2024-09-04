136b39a454d964e048531a6c41d93bb4cca65d7c
JeonEHye <grace9189@gmail.com>
2024-09-04 11:36:20 +0900

    임시 코드 작성

--- app/src/main/java/com/example/picwrite/ui/diaryentry/DiaryEntryViewModel.kt
+++ app/src/main/java/com/example/picwrite/ui/diaryentry/DiaryEntryViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.picwrite.ui.diaryentry
 
-class DiaryEntryViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.picwrite.ui.album.AlbumItem
+
+class DiaryEntryViewModel  : ViewModel() {
+
+    private val _items = MutableLiveData<List<AlbumItem>>()
+//    val items: LiveData<List<AlbumItem>>
+//        get() = _items{
 }
--- app/src/main/java/com/example/picwrite/ui/diarylist/DiaryListViewModel.kt
+++ app/src/main/java/com/example/picwrite/ui/diarylist/DiaryListViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.picwrite.ui.diarylist
 
-class DiaryListViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.picwrite.R
+import com.example.picwrite.ui.album.AlbumItem
+
+class DiaryListViewModel  : ViewModel() {
+
+    private val _items = MutableLiveData<List<AlbumItem>>()
+//    val items: LiveData<List<AlbumItem>>
+//        get() = _items
+
+//
+//    init {
+//        val itemList = mutableListOf<AlbumItem>()
+//        itemList.add(AlbumItem(R.drawable.picwrite, "1월"))
+//        itemList.add(AlbumItem(R.drawable.picwrite, "2월"))
+//        itemList.add(AlbumItem(R.drawable.picwrite, "3월"))
+//        itemList.add(AlbumItem(R.drawable.picwrite, "4월"))
+//        itemList.add(AlbumItem(R.drawable.picwrite, "5월"))
+//        itemList.add(AlbumItem(R.drawable.picwrite, "6월"))
+//
+//        _items.value = itemList
+//    }
 }
