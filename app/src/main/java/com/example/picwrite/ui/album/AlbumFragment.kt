7a6e5a6949366bd73ef371ed9c3a1810a5c0f2bd
JeonEHye <grace9189@gmail.com>
2024-09-04 11:36:02 +0900

    앨범 코드 수정

--- app/src/main/java/com/example/picwrite/ui/album/AlbumFragment.kt
+++ app/src/main/java/com/example/picwrite/ui/album/AlbumFragment.kt
@@ -28,6 +28,8 @@ class AlbumFragment : Fragment() {
         _binding = FragmentAlbumBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
+        // 앨범 상단 리사이클러뷰 달력 12개 표시
         // GridLayoutManager 설정
         val layoutManager = GridLayoutManager(requireContext(), 4)
         layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
@@ -42,6 +44,24 @@ class AlbumFragment : Fragment() {
         viewModel.items.observe(viewLifecycleOwner, Observer { items ->
             binding.albumRecyclerView.adapter = AlbumAdapter(items)
         })
+
+        // 앨범 하단 리사이클러뷰 달력 12개 표시
+        // GridLayoutManager 설정
+        val layoutManager2 = GridLayoutManager(requireContext(), 3)
+        layoutManager2.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return 1
+            }
+        }
+
+        binding.albumRecyclerView2.layoutManager = layoutManager2
+
+        // ViewModel의 데이터를 관찰하여 RecyclerView에 연결
+        viewModel.items2.observe(viewLifecycleOwner, Observer { items ->
+            binding.albumRecyclerView2.adapter = AlbumAdapter(items)
+        })
+
+
         return root
     }
 
--- app/src/main/java/com/example/picwrite/ui/album/AlbumViewModel.kt
+++ app/src/main/java/com/example/picwrite/ui/album/AlbumViewModel.kt
@@ -9,6 +9,9 @@ class AlbumViewModel : ViewModel() {
     private val _items = MutableLiveData<List<AlbumItem>>()
     val items: LiveData<List<AlbumItem>> get() = _items
 
+    private val _items2 = MutableLiveData<List<AlbumItem>>()
+    val items2: LiveData<List<AlbumItem>> get() = _items2
+
     init {
         val itemList = mutableListOf<AlbumItem>()
         itemList.add(AlbumItem(R.drawable.picwrite, "1월"))
@@ -25,5 +28,21 @@ class AlbumViewModel : ViewModel() {
         itemList.add(AlbumItem(R.drawable.picwrite, "12월"))
 
         _items.value = itemList
+
+        val itemList2 = mutableListOf<AlbumItem>()
+        itemList2.add(AlbumItem(R.drawable.picwrite, "1월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "2월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "3월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "4월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "5월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "6월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "7월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "8월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "9월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "10월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "11월"))
+        itemList2.add(AlbumItem(R.drawable.picwrite, "12월"))
+
+        _items2.value = itemList
     }
 }
